66b5b30f5647eff3aeb6c696d1c4b63719692d6e
Tiyas <tiiyasastutiningsih@gmail.com>
2024-07-27 14:29:21 +0700

    penambahan feature dari branch development

--- app.py
+++ app.py
@@ -1,1 +1,5 @@
 print('Hello world')
+def calculator():
+    num1=float(input("enter the first number"))
+    num2=float(input("enter the second number"))
+    print("The sum of the number is:", num1+num2)
